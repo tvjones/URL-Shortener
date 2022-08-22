654a6c3add5d798c70c3b5ca27e5696441ed5f18
tvjones <43976584+tvjones@users.noreply.github.com>
2022-08-21 19:10:38 -0500

    file structure

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ async function getURL() {
     inputField.className = "sm:w-[90%] sm:h-[40%] sm:top-[15%] sm:m-auto sm:block p-[1.5rem] font-bold w-[80%] h-[50%] rounded text-[#9a9a9a] top-[27%] relative placeholder:text-[#ff6666] border-[#ff6666] border-[2px]"
     errorMessage.className = "sm:w-[70%] sm:m-auto w-[80%] text-[1.5rem] relative bottom-[2vh] m-auto  text-[#ff6666] italic sm:top-[15%] block"
   } else {
-    errorMessage.className = "sm:w-[70%] sm:m-auto w-[80%] text-[1.5rem] relative bottom-[2vh] m-auto  text-[#ff6666] italic sm:top-[15%] "
+    errorMessage.className = "sm:w-[70%] sm:m-auto w-[80%] text-[1.5rem] relative bottom-[2vh] m-auto  text-[#ff6666] italic sm:top-[15%] hidden "
     inputField.className = "sm:w-[90%] sm:h-[40%] sm:top-[15%] sm:m-auto sm:block p-[1.5rem] font-bold w-[80%] h-[50%] rounded text-[#9a9a9a] top-[27%] relative"
     url = "https://api.shrtco.de/v2/shorten?url=" + longURL;
     console.log(url);
